0c1d7e210eb6efc746659f0592c53fb391be6d93
doc7720 <bodyazastavnyi97@gmail.com>
2022-04-30 13:29:55 +0300

    add homework 8 part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
+import {Navigate, Route, Routes} from "react-router-dom";
 
-    </div>
-  );
-}
+import {MainLayout} from './layouts';
+import {CarsPage, NotFoundPage, SingleCarPage} from './pages';
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<MainLayout/>}>
+                <Route index element={<Navigate to={"cars"}/>}/>
+                <Route path="cars/:carId" element={<SingleCarPage/>}/>
+                <Route path="cars" element={<CarsPage/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
---
+++ src/components/Car/Car.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+
+const Car = ({car, car: {id, model, price}}) => {
+    return (
+        <div>
+            {id} -- {model} -- {price}
+            <Link to={id.toString()} state={car}>
+                <button>Info</button>
+            </Link>
+        </div>
+    );
+};
+
+export {Car};
